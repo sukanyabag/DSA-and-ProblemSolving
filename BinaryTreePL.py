5bdbfcac9f249b62f7be23e90b831b48deba8181
Sukanya Bag <sukannya472@gmail.com>
2021-06-20 16:08:05 +0530

    implemented binary tree operations with python list

---
+++ BinaryTreePL.py
@@ -0,0 +1,72 @@
+class BinaryTree:
+    def __init__(self, size):
+        self.customList = size * [None]
+        self.maxsize = size
+        self.lastUsedIdx = 0
+
+    def insertNode(self, value): 
+        if self.lastUsedIdx + 1 == self.maxsize:
+            return "The Binary Tree is full."
+        self.customList[self.lastUsedIdx + 1] = value
+        self.lastUsedIdx += 1
+        return "The node has been successfully inserted."
+    
+    def searchNode(self, nodevalue): # O(N) time & O(1) space.
+        for i in range (len(self.customList)):
+            if self.customList[i] == nodevalue:
+                return "Node found!"
+            else:
+                return "Node not found!"
+
+    def preOrderTraversal(self, idx): # O(N) time & O(N) space.
+        if idx > self.lastUsedIdx:
+            return
+        print(self.customList[idx]) #root
+        self.preOrderTraversal(idx*2) #left
+        self.preOrderTraversal(idx*2 + 1) #right
+
+    def inOrderTraversal(self,idx): # O(N) time & O(N) space.
+        if idx > self.lastUsedIdx:
+            return
+        self.inOrderTraversal(idx*2) #left
+        print(self.customList[idx]) #root
+        self.inOrderTraversal(idx*2 + 1) #right
+    
+    def postOrderTraversal(self,idx): # O(N) time & O(N) space.
+        if idx > self.lastUsedIdx:
+            return
+        self.postOrderTraversal(idx*2) #left
+        self.postOrderTraversal(idx*2 + 1) #right
+        print(self.customList[idx]) #root
+
+    def levelOrderTraversal(self,idx): # O(N) time & O(N) space.
+        for i in range(idx, self.lastUsedIdx+1):
+            print(self.customList[i])
+
+    def deleteNode(self, value): # O(N) time & O(N) space.
+        if self.lastUsedIdx == 0:
+            return "No node found to be deleted."
+        # a)find the value we want to delete
+        #b) replace that value with the value of deepest node(here, deepest node =lastindex)
+        #c) finally delete the deepest node from the binary tree
+        for i in range(1, self.lastUsedIdx+1):
+            if self.customList[i] == value:
+                self.customList[i] = self.customList[self.lastUsedIdx]
+                self.customList[self.lastUsedIdx] = None
+                self.lastUsedIdx -= 1
+                return "The node has been successfully deleted."
+    
+    def deleteBT(self): # O(N) time & O(N) space.
+        self.customList = None
+        return "The binary tree has been deleted!"
+
+    
+newBT = BinaryTree(8)
+print(newBT.insertNode("Drinks"))
+print(newBT.insertNode("Hot"))
+print(newBT.insertNode("Cold"))
+print(newBT.searchNode("Tea"))
+newBT.preOrderTraversal(1)
+newBT.inOrderTraversal(1)
+newBT.postOrderTraversal(1)
+newBT.levelOrderTraversal(1)
