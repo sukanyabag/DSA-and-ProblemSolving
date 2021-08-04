834e1fda6e9c2bc90bb17330c0241f1c3434b7e1
Sukanya Bag <sukannya472@gmail.com>
2021-08-04 12:09:43 +0530

    Create subset_II.py

---
+++ FAANG Interview Solutions/subset_II.py
@@ -0,0 +1,33 @@
+'''
+Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ Example 1:
+
+Input: nums = [1,2,2]
+Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+ 
+Constraints:
+
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
+'''
+
+class Solution:
+    def subsetsWithDup(self, nums):
+        if not nums:
+            return []
+        nums.sort()
+        res, cur = [[]], []
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                cur = [item + [nums[i]] for item in cur]
+            else:
+                cur = [item + [nums[i]] for item in res]
+            res += cur
+        return res
