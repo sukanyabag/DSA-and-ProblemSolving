147ae0a9901cd8272074dcc4e9c249d56a669096
Sukanya Bag <sukannya472@gmail.com>
2021-08-09 11:24:58 +0530

    single source shortest path w bfs approach

---
+++ Single_src_shortestpath.py
@@ -0,0 +1,30 @@
+class Graph:
+    def __init__(self,gdict = None):
+        if gdict is None:
+            gdict = {}
+        self.gdict = gdict
+
+    def bfs_sssp(self, start, end): # time - O(E) , E = edges, space - O(E)
+        queue = []
+        queue.append([start])
+
+        while queue:
+            path = queue.pop(0)
+            node = path[-1]
+            if node ==  end:
+                return path
+            for adjacent in self.gdict.get(node, []):
+                new_path = list(path)
+                new_path.append(adjacent)
+                queue.append(new_path)
+
+customDict = { "a" : ["b", "c"],
+               "b" : ["d", "g"],
+               "c" : ["d", "e"],
+               "d" : ["f"],
+               "e" : ["f"],
+               "g" : ["f"]
+            }
+
+g = Graph(customDict)
+print(g.bfs_sssp("a", "e"))
