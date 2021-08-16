2aade36dceed9eb61eb5a84917a72ea0944a2be8
Sukanya Bag <sukannya472@gmail.com>
2021-08-16 14:14:43 +0530

    leetcode #242, Amazon

---
+++ FAANG Interview Solutions/Valid_Anagram.py
@@ -0,0 +1,20 @@
+'''
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ 
+Constraints:
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+'''
+
+class Solution:
+    def isAnagram(self, s, t):
+        return sorted(s) == sorted(t)
+        
