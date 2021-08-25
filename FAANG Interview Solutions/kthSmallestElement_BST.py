134292a6f411bc454f4cca72426fd36ea18d977b
Sukanya Bag <sukannya472@gmail.com>
2021-08-25 20:54:51 +0530

    leetcode #230, Amazon, Google, Microsoft, Apple, Uber, Facebook

---
+++ FAANG Interview Solutions/kthSmallestElement_BST.py
@@ -0,0 +1,27 @@
+'''
+Given the root of a binary search tree, and an integer k, return the kth (1-indexed) smallest element in the tree.
+
+Input: root = [3,1,4,null,2], k = 1
+Output: 1
+
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+Output: 3
+
+Constraints:
+The number of nodes in the tree is n.
+1 <= k <= n <= 104
+0 <= Node.val <= 104
+'''
+
+class Solution:
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        def inorder(r):
+            return inorder(r.left) + [r.val] + inorder(r.right) if r else []
+    
+        return inorder(root)[k - 1]
+
