8727bba24af45e52b294533518c79a6b6a5848cc
Sukanya Bag <sukannya472@gmail.com>
2021-10-11 13:10:13 +0530

    Leetcode#543, Amazon, Apple,Facebook ,Google ,Intel ,Microsoft ,Oracle,Tencent

--- FAANG Interview Solutions/diameterOfBinaryTree.py
+++ FAANG Interview Solutions/diameterOfBinaryTree.py
@@ -31,7 +31,8 @@ class Solution(object):
         self.ans = 0
         
         def depth(p):
-            if not p: return 0
+            if not p: 
+               return 0
             left, right = depth(p.left), depth(p.right)
             self.ans = max(self.ans, left+right)
             return 1 + max(left, right)
