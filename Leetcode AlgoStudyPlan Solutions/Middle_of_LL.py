034f9ab27a38ada33f82ae1ccc682dd9c0d4c681
Sukanya Bag <sukannya472@gmail.com>
2021-10-30 09:44:40 +0530

    Create Middle_of_LL.py

---
+++ Leetcode AlgoStudyPlan Solutions/Middle_of_LL.py
@@ -0,0 +1,26 @@
+'''
+Given the head of a singly linked list, return the middle node of the linked list.
+If there are two middle nodes, return the second middle node.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+
+Constraints:
+The number of nodes in the list is in the range [1, 100].
+1 <= Node.val <= 100
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        temp = head
+        while temp and temp.next:
+            head = head.next
+            temp = temp.next.next
+        return head
+        
