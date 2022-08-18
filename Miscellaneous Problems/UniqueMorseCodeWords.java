64891af195aa4fb3ce5f8ea900c8dff45a54a0b9
Sukanya Bag <sukannya472@gmail.com>
2022-08-18 12:41:11 +0530

    Create UniqueMorseCodeWords.java

---
+++ Miscellaneous Problems/UniqueMorseCodeWords.java
@@ -0,0 +1,42 @@
+/*
+Unique Morse Code Words
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+
+'a' maps to ".-",
+'b' maps to "-...",
+'c' maps to "-.-.", and so on.
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...".
+We will call such a concatenation the transformation of a word.
+Return the number of different transformations among all words we have.
+
+
+Example 1:
+Input: words = ["gin","zen","gig","msg"]
+Output: 2
+Explanation: The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+There are 2 different transformations: "--...-." and "--...--.".
+
+Constraints:
+1 <= words.length <= 100
+1 <= words[i].length <= 12
+words[i] consists of lowercase English letters.
+*/
+
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        morse_codes = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--."
+                       ,"--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        
+        uniqmorse = {"".join(morse_codes[ord(letter) - ord('a')] for letter in word) for word in words}
+            
+        
+        return len(uniqmorse)
