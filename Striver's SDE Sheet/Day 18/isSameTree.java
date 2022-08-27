59e8a64795760507843803292d912581eb321382
Sukanya Bag <sukannya472@gmail.com>
2022-08-28 00:52:56 +0530

    TC - O(N), SC- O(H)-> WORST CASE (SKEWED TREE)

---
+++ Striver's SDE Sheet/Day 18/isSameTree.java
@@ -0,0 +1,32 @@
+/*
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+Constraints:
+The number of nodes in both trees is in the range [0, 100].
+-104 <= Node.val <= 104
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null || q == null) 
+            return (p==q);
+            
+        return (p.val == q.val) && isSameTree(p.left,q.left) && isSameTree(p.right, q.right);
+    }
+}
