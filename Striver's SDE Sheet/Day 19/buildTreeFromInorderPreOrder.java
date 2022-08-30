5b52a35a001dbbc49e1515b870e15d0e2085912b
Sukanya Bag <sukannya472@gmail.com>
2022-08-30 10:03:52 +0530

    Update buildTreeFromInorderPreOrder.java

--- Striver's SDE Sheet/Day 19/buildTreeFromInorderPreOrder.java
+++ Striver's SDE Sheet/Day 19/buildTreeFromInorderPreOrder.java
@@ -83,3 +83,16 @@ class Solution {
         
     }
 }
+
+//PYTHON VERSION - 
+ class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder: return None
+        
+        root = TreeNode(preorder[0])
+        pivot = inorder.index(preorder[0])
+        
+        root.left = self.buildTree(preorder[1: pivot+1], inorder[:pivot])
+        root.right = self.buildTree(preorder[pivot+1: ], inorder[pivot+1: ])
+        
+        return root
