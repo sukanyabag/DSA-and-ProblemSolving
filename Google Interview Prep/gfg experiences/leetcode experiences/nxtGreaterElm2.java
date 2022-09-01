e8f902976b137d9e10bf90797893fff542fda470
Sukanya Bag <sukannya472@gmail.com>
2022-09-01 23:37:49 +0530

    Create nxtGreaterElm2.java

---
+++ Google Interview Prep/gfg experiences/leetcode experiences/nxtGreaterElm2.java
@@ -0,0 +1,49 @@
+/*
+
+Given a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]), return the next greater number for every element in nums.
+
+The next greater number of a number x is the first greater number to its traversing-order next in the array, 
+which means you could search circularly to find its next greater number. If it doesn't exist, return -1 for this number.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,1]
+Output: [2,-1,2]
+Explanation: The first 1's next greater number is 2; 
+The number 2 can't find next greater number. 
+The second 1's next greater number needs to search circularly, which is also 2.
+Example 2:
+
+Input: nums = [1,2,3,4,3]
+Output: [2,3,4,-1,4]
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-109 <= nums[i] <= 109
+
+
+Explanation
+Loop once, we can get the Next Greater Number of a normal array.
+Loop twice, we can get the Next Greater Number of a circular array
+
+
+Complexity
+Time O(N) for one pass
+Spce O(N) in worst case
+*/
+
+public int[] nextGreaterElements(int[] A) {
+        int n = A.length, res[] = new int[n];
+        Arrays.fill(res, -1);
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n * 2; i++) {
+            while (!stack.isEmpty() && A[stack.peek()] < A[i % n])
+                res[stack.pop()] = A[i % n];
+            stack.push(i % n);
+        }
+        return res;
+    }
