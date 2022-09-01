7f18e783a98b1a1726927f24c20097d7be81134a
Sukanya Bag <sukannya472@gmail.com>
2022-09-01 23:29:48 +0530

    O(N)

---
+++ Google Interview Prep/gfg experiences/leetcode experiences/nextGreaterElem.java
@@ -0,0 +1,66 @@
+/**
+The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
+
+You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+
+For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. 
+If there is no next greater element, then the answer for this query is -1.
+
+Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+
+Example 1:
+
+Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+Output: [-1,3,-1]
+Explanation: The next greater element for each value of nums1 is as follows:
+- 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+- 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
+- 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+
+Constraints:
+
+1 <= nums1.length <= nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 104
+All integers in nums1 and nums2 are unique.
+All the integers of nums1 also appear in nums2.
+ 
+
+Follow up: Could you find an O(nums1.length + nums2.length) solution?
+**/
+
+public int[] nextGreaterElement(int[] findNums, int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>(); // map from x to next greater element of x
+        Stack<Integer> stack = new Stack<>();
+        for (int num : nums) {
+            while (!stack.isEmpty() && stack.peek() < num)
+                map.put(stack.pop(), num);
+            stack.push(num);
+        }   
+        for (int i = 0; i < findNums.length; i++)
+            findNums[i] = map.getOrDefault(findNums[i], -1);
+        return findNums;
+    }
+
+
+ALTERNATE - 
+  Given a list of numbers, replace every candidate with the next greater element for every element.
+    
+    public static void getNGE(int[] a) {
+    Stack<Integer> s = new Stack<Integer>();
+    s.push(a[0]);
+
+    for (int i = 1; i < a.length; i++) {
+        if (s.peek() != null) {
+            while (true) {
+                if (s.peek() == null || s.peek() > a[i]) {
+                    break;
+                }
+                System.out.println(s.pop() + ":" + a[i]);
+            }
+        }
+        s.push(a[i]);
+    }
+    while (s.peek() != null) {
+        System.out.println(s.pop() + ":" + -1);
+    }
+}
