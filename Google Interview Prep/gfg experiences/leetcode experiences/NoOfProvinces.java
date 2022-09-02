b2063489b2457099a54953ed8077f5063207d5b7
Sukanya Bag <sukannya472@gmail.com>
2022-09-02 10:21:30 +0530

    Create NoOfProvinces.java

---
+++ Google Interview Prep/gfg experiences/leetcode experiences/NoOfProvinces.java
@@ -0,0 +1,34 @@
+/*
+SIMILAR TO FRIEND CIRCLES
+
+There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected
+directly with city c, then city a is connected indirectly with city c.
+
+A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+
+You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+
+Return the total number of provinces.
+*/
+
+public class Solution {
+    public void dfs(int[][] M, int[] visited, int i) {
+        for (int j = 0; j < M.length; j++) {
+            if (M[i][j] == 1 && visited[j] == 0) {
+                visited[j] = 1;
+                dfs(M, visited, j);
+            }
+        }
+    }
+    public int findCircleNum(int[][] M) {
+        int[] visited = new int[M.length];
+        int count = 0;
+        for (int i = 0; i < M.length; i++) {
+            if (visited[i] == 0) {
+                dfs(M, visited, i);
+                count++;
+            }
+        }
+        return count;
+    }
+}
