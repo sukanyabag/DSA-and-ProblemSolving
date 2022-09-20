2db8d9c72b1481224a599f383e97ecb422c70767
Sukanya Bag <sukannya472@gmail.com>
2022-09-20 11:45:17 +0530

    video link in description
    https://www.youtube.com/watch?v=lXVy6YWFcRM

---
+++ Striver's SDE Sheet/Day 25/maxProdSubarray.py
@@ -0,0 +1,25 @@
+'''
+Problem Statement: Given an array that contains both negative and positive integers, find the maximum product subarray.
+
+Examples:
+Example 1:
+Input:
+ Nums = [1,2,3,4,5,0]
+Output:
+ 120
+Explanation:
+ In the given array, we can see 1×2×3×4×5 gives maximum product value.
+'''
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        # O(n)/O(1) : Time/Memory
+        res = nums[0]
+        curMin, curMax = 1, 1
+
+        for n in nums:
+
+            tmp = curMax * n
+            curMax = max(n * curMax, n * curMin, n)
+            curMin = min(tmp, n * curMin, n)
+            res = max(res, curMax)
+        return res
