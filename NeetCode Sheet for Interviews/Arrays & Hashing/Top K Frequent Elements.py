b807dd94e10b84580dfeaa05fc966c74420f4da9
Sukanya Bag <sukannya472@gmail.com>
2022-10-12 14:28:25 +0530

    Create Top K Frequent Elements.py

---
+++ NeetCode Sheet for Interviews/Arrays & Hashing/Top K Frequent Elements.py
@@ -0,0 +1,34 @@
+'''
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+Example 1:
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+
+Constraints:
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+k is in the range [1, the number of unique elements in the array].
+It is guaranteed that the answer is unique.
+ 
+Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+'''
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+
+        # O(n)
