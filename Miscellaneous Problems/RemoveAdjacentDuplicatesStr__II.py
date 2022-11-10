c9e1120ecfe8477769ef465344bbbe176fc0dec1
Sukanya Bag <sukannya472@gmail.com>
2022-11-10 09:45:57 +0530

    Create RemoveAdjacentDuplicatesStr__II.py

---
+++ Miscellaneous Problems/RemoveAdjacentDuplicatesStr__II.py
@@ -0,0 +1,20 @@
+'''
+Save the character c and its count to the stack.
+If the next character c is same as the last one, increment the count.
+Otherwise push a pair (c, 1) into the stack.
+use a dummy element ('#', 0) to avoid empty stack.
+
+Time O(N) for one pass
+Space O(N)
+'''
+
+    def removeDuplicates(self, s, k):
+        stack = [['#', 0]]
+        for char in s:
+            if stack[-1][0] == char:
+                stack[-1][1] += 1
+                if stack[-1][1] == k:
+                    stack.pop()
+            else:
+                stack.append([char, 1])
+        return ''.join(char * k for char, k in stack)
