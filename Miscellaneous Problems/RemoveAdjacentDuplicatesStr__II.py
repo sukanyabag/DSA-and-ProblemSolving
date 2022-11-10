d8eec63f6b20aa3b9d2142bef666e0b0d8a4962f
Sukanya Bag <sukannya472@gmail.com>
2022-11-10 09:50:32 +0530

    Update RemoveAdjacentDuplicatesStr__II.py

--- Miscellaneous Problems/RemoveAdjacentDuplicatesStr__II.py
+++ Miscellaneous Problems/RemoveAdjacentDuplicatesStr__II.py
@@ -1,8 +1,8 @@
 '''
-Save the character c and its count to the stack.
-If the next character c is same as the last one, increment the count.
-Otherwise push a pair (c, 1) into the stack.
-use a dummy element ('#', 0) to avoid empty stack.
+Save the character char and its count to the stack.
+If the next character char is same as the last one, increment the count.
+Otherwise push a pair (char, 1) into the stack.
+use a dummy element ('$', 0) to avoid empty stack.
 
 Time O(N) for one pass
 Space O(N)
